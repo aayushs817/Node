326d3617c51c75a37e2324b0f0c1fbac0d71b89b
yatri <yatri@solulab.co>
2020-04-28 00:42:52 +0530

    Simple node command

---
+++ index.js
@@ -0,0 +1,21 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+}
+ 
+function solverect(l,b){
+    console.log("Solving rectangle with l = "+l+" and b = "+b);
+
+    if(l <= 0 || b<= 0){
+        console.log("The value should be greater than zero l = "+l+ " and b = "+b);
+    }
+    else{
+        console.log("The area of rectangle is = " +rect.area(l,b));
+        console.log("The perimeter of rectangle is = " +rect.perimeter(l,b));
+    }
+}
+
+solverect(2,4);
+solverect(3,5);
+solverect(0,5);
+solverect(-1,-2);
